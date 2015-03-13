2e202f9cab82d705a5b584c26bd2b215eb24c327
Huck Nuchelmans <huck.nuchelmans@gmail.com>
2015-03-13 14:29:57 +0100

    add devil mode

--- nightcore.py
+++ nightcore.py
@@ -8,25 +8,56 @@ from moviepy.audio.io.AudioFileClip import AudioFileClip
 from moviepy.video.VideoClip import TextClip, ImageClip
 from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
 
-from random import choice
+from random import choice, random
 from os import listdir
 
 
 def speedup_audio(sound_array, factor):
     """
-    Multiplies the sound's speed by some factor (including pitch shift).
+    Multiply the sound's speed by some factor (including pitch shift).
     source: http://goo.gl/MlI9UM
     """
 
-    # Pick new indices with the factor as interval
+    # Pick new indices with the factor as interval.
     indices = np.round(np.arange(0, len(sound_array), factor))
     indices = indices[indices < len(sound_array)].astype(int)
 
-    # return a new array with just the data from the calculated indices
+    # return a new array with just the data from the calculated indices.
     return sound_array[indices.astype(int)]
 
 
-def prepare_audio(file_name, rate=44100, speedup=1.3):
+def dance_with_the_devil(sound_array, rate, glitches=0.25,
+                         glitch_duration=1.00, volume=100):
+    """
+    Ruin the sound_array by adding random glitches, where the sound is sped up
+    or slowed down. Also raise the volume.
+    """
+
+    # Create an array with all moments a glitch can occur.
+    glitch_ops = np.arange(1, int(len(sound_array) / rate),
+                           glitch_duration * 2)
+
+    # Pick a fraction (the variable "glitches") of these opportunities
+    np.random.shuffle(glitch_ops)
+    splits = np.sort(glitch_ops[:int(len(glitch_ops) * glitches)])
+
+    # Add the end of each glitch to the array and multiply with the rate so we
+    # have the actual array indices.
+    splits = np.insert(splits, np.arange(1, len(splits)),
+                       np.array([s + glitch_duration for s in splits])) * rate
+
+    # Split the audio so glitch pieces can be edited separately.
+    pieces = np.split(sound_array, splits)
+
+    # Speed up or slow down glitch pieces randomly.
+    for i in np.arange(1, len(pieces), 2):
+        pieces[i] = speedup_audio(pieces[i], random() * 1.5 + 0.5)
+
+    # Put all pieces back together and multiply with volume.
+    return np.concatenate(pieces) * volume
+
+
+def prepare_audio(file_name, rate=44100, speedup=1.3, devil=False):
     """
     Read the audio from the input file, speed it up and return it as an
     AudioArrayClip.
@@ -37,7 +68,13 @@ def prepare_audio(file_name, rate=44100, speedup=1.3):
     data = audio.to_soundarray(fps=rate)
 
     # Speedup the the data in the array and return it as an audio clip.
-    return AudioArrayClip(speedup_audio(data, speedup), fps=rate)
+    data = speedup_audio(data, speedup)
+
+    # If devil mode is enabled, dance, with the devil.
+    if devil:
+        data = dance_with_the_devil(data, rate)
+
+    return AudioArrayClip(data, fps=rate)
 
 
 def prepare_image(text, duration, fps=1):
@@ -59,14 +96,14 @@ def prepare_image(text, duration, fps=1):
     return CompositeVideoClip([image, text]).set_duration(duration)
 
 
-def nightcorify(source_file, destination_file, text):
+def nightcorify(source_file, destination_file, text, devil=False):
     """
     Create a nightcore video using the audio from source_file and write it to
     destination_file. Draw the text on top of the video.
     """
 
     # Read the audio from our source file.
-    audio = prepare_audio(source_file)
+    audio = prepare_audio(source_file, devil=devil)
 
     # Create a random image with the user text on it and set its duration to
     # the duration of the sped up audio.
@@ -81,7 +118,9 @@ if __name__ == "__main__":
     from sys import argv, exit
 
     if len(argv) < 4:
-        print "usage: nightcore.py <inputfile.wav> <outputfile.avi> <title>"
+        print "usage: nightcore.py <inputfile.wav> <outputfile.avi> " + \
+              "<title> [-d]"
         exit(1)
 
-    nightcorify(argv[1], argv[2], argv[3])
+    devil = (len(argv) > 4 and argv[4].lower == "-d")
+    nightcorify(argv[1], argv[2], argv[3], devil)
